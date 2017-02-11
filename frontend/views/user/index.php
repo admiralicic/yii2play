75a25f742b4ba080aca4d91a38d86106d9078833
Admir Alicic <admir_alicic@yahoo.com>
2017-02-11 18:24:34 +0100

    initial commit

---
+++ frontend/views/user/index.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use \yii\helpers\ArrayHelper;
+use \common\models\User;
+
+/* @var $this yii\web\View */
+/* @var $searchModel frontend\models\UserSearchfr */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Users';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php //echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create User', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+//            'username',
+            'email:email',
+            'status',
+            [
+                'attribute' => 'username',
+                'format' => 'raw',
+                'value' => function ($model) {
+                    return Html::activeDropDownList($model, 'username',
+                        ArrayHelper::map(User::find()->all(), 'username', 'username'),
+                        [
+                            'class' => 'form-control',
+                            'onchange' => 'alert("change ' . $model->username . '")',
+
+
+                        ]);
+                },
+                'filter' => ArrayHelper::map(User::find()->all(), 'username', 'username')
+
+            ],
+//            'auth_key',
+//            'password_hash',
+//            'password_reset_token',
+            'created_at:datetime',
+            // 'updated_at',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+        'layout' => '{pager}{items}{summary}{pager}'
+    ]); ?>
+</div>
